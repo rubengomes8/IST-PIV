b6f19ff7d869878ee3798dc5acdb715988300316
John Mendonca <33038989+mfsmm@users.noreply.github.com>
2018-11-28 22:49:09 +0000

    Add files via upload

---
+++ Part1/update_objects.m
@@ -0,0 +1,26 @@
+function connection = update_objects(objects, box, im_hsv )
+%Compares previous known objects with current boxed object; return
+%connection if exists
+%   for example, if box(1) is connected to object 2 then connection(1) = 2
+%   if there is no connection then connection(i) = 0;
+
+
+% we use the box center of mass and image histogram and compare with the
+% object center of mass(needs to be calculated here because it is not
+% included in the struct) and color histogram
+
+connection = zeros(1,length(box));
+
+for i = 1:length(box)
+    for j = 1:length(objects)
+        
+        %calculate cost(color+distance)
+        cost(j) = 
+    end
+    [best,n] = min(cost);
+    if best < 0.1 %threshold. We need to be careful with the possibility of having 2 boxes choosing 1 object
+        connection(i) = n;
+    end    
+   
+end
+
