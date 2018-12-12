cce56dda51594b0eadcf7592b85ced463f618819
Rúben Gomes <30573326+rubengomes8@users.noreply.github.com>
2018-12-12 19:05:27 +0000

    Add files via upload

--- Part2/InicializarProjecto.m
+++ Part2/InicializarProjecto.m
@@ -1,12 +1,18 @@
 close all;
 load('cameraparametersAsus.mat');
 %store all .jpeg and .mat files into variables
-d=dir('*.jpg');
+d=dir('*.png');
 dd=dir('*.mat');
-for i=1:length(d)
+for i=1:length(d)/2
     imgseq1(i).rgb = d(i).name;
     imgseq1(i).depth = dd(i+1).name;
 end
+a = 1;
+for i = length(d)/2+1:length(d)
+    imgseq2(a).rgb = d(i).name;
+    imgseq2(a).depth = dd(i+1).name;
+    a=a+1;
+end
 tic
-objects = track3D_part1(imgseq1, cam_params);
+[objects, cam2toW] = track3D_part2( imgseq1, imgseq2, cam_params);
 toc
