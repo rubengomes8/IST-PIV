c5da826538f1cf5a030968f4534cee7128fc8aec
Rúben Gomes <30573326+rubengomes8@users.noreply.github.com>
2018-12-12 14:09:52 +0000

    Add files via upload

--- Part1/add_object.m
+++ Part1/add_object.m
@@ -9,22 +9,22 @@ function [box,prev_frame_box]=add_object(box,prev_frame_box,aux, max_z_bg)
             %color = color_dist(box.hist(i,:,:),prev_frame_box.hist(j,:,:));
             cost(j) = xyz;
         end
-        [best,n] = min(cost);
+        [best,n] = min(cost); %min cost for object i 
         if best < 0.1 %threshold. We need to be careful with the possibility of having 2 boxes choosing 1 box
             %add new object
-            box.connection(i) = n;
-            prev_frame_box.connection(n) = i;
-            if aux == 0
-                box.nr(i) = number;
-                prev_frame_box.nr(n) = number;
+            box.connection(i) = n; %object i is connected to object n of the previous box
+            prev_frame_box.connection(n) = i; %object of previous box n is connected to the object i
+            if aux == 0 %1st connection
+                box.nr(i) = number; %number of the object
+                prev_frame_box.nr(n) = number; 
                 number = number+1;
             else
-                if prev_frame_box.nr(n) == 0
+                if prev_frame_box.nr(n) == 0 %adiction of a new object
                     number = max(prev_frame_box.nr)+1;
                     box.nr(i) = number;
                     prev_frame_box.nr(n) = number;
                 else
-                    box.nr(i) = prev_frame_box.nr(n);
+                    box.nr(i) = prev_frame_box.nr(n); %only the current object (box) is updated
                 end
             end
                 
