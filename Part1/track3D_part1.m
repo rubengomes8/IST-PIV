da9aa6dca892dd333cc6c592cbd60c3236d6e3a6
Rúben Gomes <30573326+rubengomes8@users.noreply.github.com>
2018-12-12 14:33:24 +0000

    Add files via upload

--- Part1/track3D_part1.m
+++ Part1/track3D_part1.m
@@ -5,12 +5,13 @@ function objects = track3D_part1(imgseq1, cam_params )
 objects = [];
 aux = 0;
 indice = 1;
+max_z_bg = max(max(res_xyz_median));
 for i=1:length(imgseq1(:))
     
     im_diff2 = abs(r(i).res_xyz(:,:,3)-res_xyz_median)>.5;
-    im_diffiltered2 = imopen(im_diff2,strel('disk',3));
+    im_diffiltered2=imopen(im_diff2,strel('disk',3));
     im_diffiltered2 = segment(im_diffiltered2,r(i).res_xyz(:,:,3));
-    [label, nr_obj] = bwlabel(im_diffiltered2,4);
+    [label, nr_obj] = bwlabel(im_diffiltered2);
     
     %Background image
     figure(1);imagesc(res_xyz_median);
@@ -19,7 +20,7 @@ for i=1:length(imgseq1(:))
     %Labeled Connected components
     figure(3);imagesc(label);
 
-    %%
+    
     %this should output the box coordinates of i objects and their centers of mass in xyz
     %meters
     % caso nao haja objectos na primeira imagem, fazer confirmação qual a
@@ -27,8 +28,7 @@ for i=1:length(imgseq1(:))
 
    box = get_box(label, nr_obj, r(i));
 
-   %%
-    if indice == 1 % first frame, store all objects!       
+    if indice == 1% first frame, store all objects!       
         prev_frame_box = box;
         indice = 0;
     elseif length(prev_frame_box.Z)<1
@@ -38,20 +38,20 @@ for i=1:length(imgseq1(:))
         prev_frame_box = box;
         continue
     else
-        [box,prev_frame_box] = add_object(box,prev_frame_box,aux);
+        [box,prev_frame_box] = add_object(box,prev_frame_box,aux,max_z_bg);
         aux = 1;
         for a = 1:length(prev_frame_box.X(:,1))
             if prev_frame_box.connection(a) ~= 0
-                obj(prev_frame_box.nr(a)).X(i-1,:)=prev_frame_box.X(a,:);
-                obj(prev_frame_box.nr(a)).Y(i-1,:)=prev_frame_box.Y(a,:);
-                obj(prev_frame_box.nr(a)).Z(i-1,:)=prev_frame_box.Z(a,:);
+                obj(prev_frame_box.nr(a)).X(i-1,:) = prev_frame_box.X(a,:);
+                obj(prev_frame_box.nr(a)).Y(i-1,:) = prev_frame_box.Y(a,:);
+                obj(prev_frame_box.nr(a)).Z(i-1,:) = prev_frame_box.Z(a,:);
             end
         end
         for a = 1:length(box.X(:,1))
             if box.connection(a) ~= 0
-                obj(box.nr(a)).X(i,:)=box.X(a,:);
-                obj(box.nr(a)).Y(i,:)=box.Y(a,:);
-                obj(box.nr(a)).Z(i,:)=box.Z(a,:);
+                obj(box.nr(a)).X(i,:) = box.X(a,:);
+                obj(box.nr(a)).Y(i,:) = box.Y(a,:);
+                obj(box.nr(a)).Z(i,:) = box.Z(a,:);
             end
         end
         prev_frame_box = box;
