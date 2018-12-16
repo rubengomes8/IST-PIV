c0be57a3431581bedcd401f2b42f57bda142c253
John Mendonca <33038989+mfsmm@users.noreply.github.com>
2018-12-16 13:17:36 +0000

    Color checking

--- Part1/add_object.m
+++ Part1/add_object.m
@@ -6,11 +6,11 @@ function [box,prev_frame_box]=add_object(box,prev_frame_box,aux, max_z_bg,obj)
         for j = 1:length(prev_frame_box.X(:,1))
             %calculate cost(color+distance)
             xyz = xyz_dist(box.cm(i,:),prev_frame_box.cm(j,:), max_z_bg);
-            %color = color_dist(box.hist(i,:,:),prev_frame_box.hist(j,:,:));
-            cost(j) = xyz;
+            color = color_dist(box.hist(i,:,:),prev_frame_box.hist(j,:,:));
+            cost(j) = xyz+color;
         end
         [best,n] = min(cost); %min cost for object i 
-        if best < 0.1 %threshold. We need to be careful with the possibility of having 2 boxes choosing 1 box
+        if best < 0.2 %threshold. We need to be careful with the possibility of having 2 boxes choosing 1 box
             %add new object
             box.connection(i) = n; %object i is connected to object n of the previous box
             prev_frame_box.connection(n) = i; %object of previous box n is connected to the object i
